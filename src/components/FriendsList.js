3fb976fb23e7b87236c319fc032f435e9a31ef41
Liam Killion <liam.killion@greenhouse.io>
2019-02-11 22:18:20 -0500

    trying typescript conversion again, remembering to use yarn

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -5,7 +5,7 @@ class FriendsList extends React.Component {
   render() {
     const friendsList = this.props.friends.map(friend => {
       let friendHangs = this.props.hangs.filter(hang=>hang.friend_id===parseInt(friend.id))
-      return (<FriendLineItem key={friend.id} friendHangs={friendHangs} friend={friend} handleCreateHang={this.props.handleCreateHang}  />);
+      return (<FriendLineItem friendHangs={friendHangs} friend={friend} handleCreateHang={this.props.handleCreateHang}  />);
     });
     return (
       <div className="friendsList">
