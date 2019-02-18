4b77b254504ecdafe4a2ff38732f46be6238557a
Liam Killion <liam.killion@greenhouse.io>
2019-02-18 17:26:46 -0500

    timeline working! next is button to hide either

--- src/components/FutureHangsList.js
+++ src/components/FutureHangsList.js
@@ -1,17 +1,32 @@
 import React from 'react'
-import HangHistoryLineItem from './HangHistoryLineItem.js'
+import Moment from 'react-moment';
+import { FaCalendarCheck,  FaRegCalendarTimes} from 'react-icons/fa'
+import { Timeline, TimelineItem }  from 'vertical-timeline-component-for-react';
 
 class UpcomingHangsList extends React.Component {
   render () {
     const orderedHangs = this.props.hangs.sort((a,b) => (a.last_nom > b.last_nom) ? 1 : ((b.last_nom > a.last_nom) ? -1 : 0));
     const historicOrderedHangs = orderedHangs.filter(hang=>new Date(hang.date) > new Date(Date.now()))
     const futureHangsList = this.props.hangs.map(hang => {
-      return (<HangHistoryLineItem key={hang.id} hang={hang} friend={hang.friend_id} />);
+      return (
+        <TimelineItem
+          key={this.props.hangs.indexOf(hang)}
+          dateText={hang.date}
+          style={{ color: '#e86971' }}
+        >
+          <p>Date: <Moment format="MM/DD/YY">{hang.date}</Moment></p>
+          <p>Activity: {hang.activity}</p>
+          <p>Did you meet up? {(hang.didHang ? <FaCalendarCheck /> : <FaRegCalendarTimes />)}</p>
+        </TimelineItem>
+      );
     });
+
     return(
       <div>
         <h5>Upcoming Hangs</h5>
-        {futureHangsList}
+        <Timeline lineColor={'#ddd'}>
+          {futureHangsList}
+        </Timeline>
       </div>
     )
   }
--- src/components/PastHangsList.js
+++ src/components/PastHangsList.js
@@ -1,20 +1,33 @@
 import React from 'react'
-import HangHistoryLineItem from './HangHistoryLineItem.js'
+import Moment from 'react-moment';
+import { FaCalendarCheck,  FaRegCalendarTimes} from 'react-icons/fa'
+import { Timeline, TimelineItem }  from 'vertical-timeline-component-for-react';
 
 class PastHangsList extends React.Component {
   render () {
     const orderedHangs = this.props.hangs.sort((a,b) => (a.last_nom > b.last_nom) ? 1 : ((b.last_nom > a.last_nom) ? -1 : 0));
     const historicOrderedHangs = orderedHangs.filter(hang=>new Date(hang.date) < new Date(Date.now()))
-    const pastHangsList = this.props.hangs.map(hang => {
-      return (<HangHistoryLineItem key={hang.id} hang={hang} friend={hang.friend_id} />);
+    const futureHangsList = this.props.hangs.map(hang => {
+      return (
+        <TimelineItem
+          key={this.props.hangs.indexOf(hang)}
+          dateText={hang.date}
+          style={{ color: '#e86971' }}
+        >
+          <p>Date: <Moment format="MM/DD/YY">{hang.date}</Moment></p>
+          <p>Activity: {hang.activity}</p>
+          <p>Did you meet up? {(hang.didHang ? <FaCalendarCheck /> : <FaRegCalendarTimes />)}</p>
+        </TimelineItem>
+      );
     });
 
     return(
       <div>
         <h5>Past Hangs</h5>
-        {pastHangsList}
+        <Timeline lineColor={'#ddd'}>
+          {futureHangsList}
+        </Timeline>
       </div>
-
     )
   }
 }
