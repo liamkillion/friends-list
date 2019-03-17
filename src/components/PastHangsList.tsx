c09705c905e264eb3b76a0a6ea749035545fc8b0
Liam Killion <liam.killion@greenhouse.io>
2019-03-17 17:54:09 -0400

    components all converted to typescript, next is app.js and getting green

---
+++ src/components/PastHangsList.tsx
@@ -0,0 +1,26 @@
+// import React from 'react'
+import * as React from 'react'
+import HangHistoryLineItem from './HangHistoryLineItem'
+import Hang from "../interfaces.ts"
+
+interface Props {hangs:Hang[]}
+
+export class PastHangsList extends React.Component<Props> {
+  render () {
+    const orderedHangs = this.props.hangs.sort((a,b) => (a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0));
+    const historicOrderedHangs = orderedHangs.filter(hang=>new Date(hang.date) < new Date(Date.now()))
+    const pastHangsList = this.props.hangs.map(hang => {
+      return (<HangHistoryLineItem key={hang.id} hang={hang} />);
+    });
+
+    return(
+      <div>
+        <h5>Past Hangs</h5>
+        {pastHangsList}
+      </div>
+
+    )
+  }
+}
+
+// export default PastHangsList;
