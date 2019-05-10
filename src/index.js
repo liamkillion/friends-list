5a2081b370a0fdc6149555a91ff900005426f3c3
Liam Killion <liam.killion@greenhouse.io>
2019-05-10 16:30:11 -0400

    getting App to load

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from "react";
+import * as React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./App.tsx";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
+
+// https://codeburst.io/webpack-typescript-react-part-1-dc154e250f23
